e01b050201200e31b7369e92090274729ada6785
Oskari Myllykylä <oskari.myllykyla@gmail.com>
2016-11-17 20:27:25 +0200

    Refactor command line argument processing
    Move some state initializing code out from the argument processing loop
     to reduce chance of memory errors. Also add command line help text.

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "ui.h"
 #include "curses/init.h"
 #include "sdl/init.h"
 #include "states/states.h"
+
+static char* generalHelp =
+"Usage: tetr [options]\n \
+General Options:\n \
+  --help\t\t\tDisplay this information\n \
+  --demo, -d <path>\t\tPlay given demo record\n \
+  --randomiser, -r <name>\tSet randomiser used. Where name is 7bag, tgm or random\n \
+  --UI <UI>\t\t\tSet UI used\n";
 
 int MainProgram(int argc, char** argv) {
     void* (*CurrentState)(UI_Functions*, void**);
@@ -19,6 +28,7 @@ int MainProgram(int argc, char** argv) {
     UI_Functions ui = {NULL};
 
     CurrentState = NULL;
+    unsigned stateArgs = 0; // index of state arguments in argv
     state_game_data gameSettings = {.randomiser = RANDOMISER_TGM};
 
     //  Game state is default
@@ -26,63 +36,85 @@ int MainProgram(int argc, char** argv) {
         CurrentState = StateGame;
     }
 
-    //  Process arguments
+    //  Process command line arguments
     for (int i=1; i<argc; i++) {
-        if (!strcmp(argv[i], "--demo")) {
-            if (argc <= ++i || *data != NULL) {
-                fprintf(stderr, "Check arguments\n--demo <path>\n");
-                CurrentState = NULL;
-                break;
-            }
-
-            //  Allocate memory and copy path
-            size_t len = strlen(argv[i]);
-            *data = malloc(sizeof(char)*len+1);
-            strcpy((char*)*data, argv[i]);
-            CurrentState = StatePlayDemo; // Set state to PlayDemo
-        }
-        else if (!strcmp(argv[i], "--randomiser")) {
+        bool invalidArgs = false;
+        if (!strcmp(argv[i], "--demo") || !strcmp(argv[i], "-d")) {
             if (argc <= ++i) {
-                fprintf(stderr, "Check arguments:\n--randomiser <name>\tWhere name is 7bag, tgm or random\n");
-                CurrentState = NULL;
-                break;
+                invalidArgs = true;
+            } else {
+                stateArgs = i; //  Save the argument index of demo path
+                CurrentState = StatePlayDemo; // Set state to PlayDemo
+            }
+        }
+        else if (!strcmp(argv[i], "--randomiser") || !strcmp(argv[i], "-r")) {
+            if (argc <= ++i) {
+                invalidArgs = true;
             } else if (!strcmp(argv[i], "7bag")) {
                 gameSettings.randomiser = RANDOMISER_BAG;
             } else if (!strcmp(argv[i], "tgm")) {
                 gameSettings.randomiser = RANDOMISER_TGM;
             } else if (!strcmp(argv[i], "random")) {
                 gameSettings.randomiser = RANDOMISER_RANDOM;
+            } else {
+                invalidArgs = true;
             }
         }
-        //  Select SDL UI
-        else if (!strcmp(argv[i], "--SDL") && !ui.UIGameInit) {
-            UIInitFun = UI_SDLInit;
+        //  Select UI
+        else if (!strcmp(argv[i], "--UI")) {
+            if (argc <= ++i) {
+                invalidArgs = true;
+            } else if (!strcmp(argv[i], "SDL")) {
+                UIInitFun = UI_SDLInit;
+            } else {
+                invalidArgs = true;
+            }
+        } else {
+            invalidArgs = true;
+        }
+
+        //  In case of invalid arguments, print help and quit
+        if (invalidArgs) {
+            fprintf(stderr, "Check arguments!\n");
+            printf("%s", generalHelp);
+            CurrentState = NULL;
+            break;
         }
     }
 
+    //  Set state specific settings
     if (CurrentState == StateGame) {
         //  Copy game settings
         if (*data != NULL) free(*data);
         state_game_data* set = (state_game_data*)malloc(sizeof(state_game_data));
         *set = gameSettings;
         *data = (void*)set;
+    } else if (CurrentState == StatePlayDemo) {
+        //  Allocate memory and copy demo path
+        size_t len = strlen(argv[stateArgs]);
+        *data = malloc(sizeof(char)*len+1);
+        strcpy((char*)*data, argv[stateArgs]);
     }
 
     //  Default UI is curses
     if (!UIInitFun) UIInitFun = CursesInit;
-    if (UIInitFun(&ui, argc, argv) != 0) {
+    //  If next state is valid, init UI
+    if (CurrentState && UIInitFun(&ui, argc, argv) != 0) {
         fprintf(stderr, "ERROR: UI init failed!\n");
         CurrentState = NULL;
     }
 
+    // Main loop
     while (CurrentState != NULL) {
         CurrentState = CurrentState(&ui, data);
 
         ui.UIMainLoopEnd(&ui);
     }
 
+    //  Make sure state data is freed
     if (*data != NULL) free(*data);
     free(data);
+    //  Run UI specific clean up code
     if (ui.UICleanup) ui.UICleanup(&ui);
 
     return 0;
